7e091e33df3b190e82a3e97c35c87b0330189e3e
张振轩 <1428425759@qq.com>
2019-09-22 16:27:12 +0800

    (docs)ipynb: 逻辑回归和决策树CART

---
+++ sklearn_logistic/sklearn_logistic.py
@@ -0,0 +1,11 @@
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+from sklearn.datasets import fetch_openml
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.utils import check_random_state
+
+print(__doc__)
